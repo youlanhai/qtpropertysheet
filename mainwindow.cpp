1c163e23da1e051b6e64b435e9d0cb89560dd155
游蓝海 <you_lan_hai@foxmail.com>
2015-11-21 12:55:32 +0800

    test create tree

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTreeWidget>
+#include <QHBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QHBoxLayout *layout = new QHBoxLayout();
+
+    QTreeWidget *widget = new QTreeWidget(ui->centralWidget);
+    widget->setColumnCount(2);
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    item->setText(0, "name");
+    item->setText(1, "value");
+    widget->addTopLevelItem(item);
+
+    layout->addWidget(widget);
+    ui->centralWidget->setLayout(layout);
 }
 
 MainWindow::~MainWindow()
